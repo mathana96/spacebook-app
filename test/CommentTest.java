ee928bd75f9763ad4ab636c5f3eae4ea272b549a
Mathana Nair <mathana96@gmail.com>
2016-04-11 20:53:51 +0100

    Added test for multiple comments (Comments from 2 users on another users post)

--- test/CommentTest.java
+++ test/CommentTest.java
@@ -6,9 +6,9 @@ import models.*;
 
 public class CommentTest extends UnitTest 
 {
-	private User bob, joe;
-	private Comment comment1, comment2;
-	private Post post1, post2;
+	private User bob, joe, lee;
+	private Comment comment1, comment2, comment3, comment4;
+	private Post post1, post2, post3, post4, post5;
 	
   @Before
   public void setup()
@@ -18,6 +18,8 @@ public class CommentTest extends UnitTest
   	bob.save();
   	joe = new User ("joe", "jones", "joe@jones.com", "secret", 20, "irish");
   	joe.save();
+  	lee = new User ("lee", "jones", "lee@jones.com", "secret", 20, "irish");
+  	lee.save();
   }
 
   @After
@@ -62,7 +64,7 @@ public class CommentTest extends UnitTest
   	//Delete it from the database
   	post.delete();
   	
-  } 
+  }
   
   @Test
   public void testDeleteComment()
@@ -95,6 +97,53 @@ public class CommentTest extends UnitTest
   	assertEquals(0, user.posts.size()); //check if no posts
   	
   	user.save(); //update user
+  	
+  	post.delete();
+  }
+  
+  @Test
+  public void testMultipleComments()
+  {
+  	post3 = new Post("Post Title 3", "The content");
+  	bob.posts.add(post3);
+  	//post4 = new Post("Post Title 4", "This is the second post content");
+  	//bob.posts.add(post3);
+  	
+  	comment3 = new Comment(joe, "Comment from Joe");
+  	comment4 = new Comment(lee, "Comment from Lee");
+  	post3.comments.add(comment3);
+  	post3.comments.add(comment4);
+  	post3.save();
+  	
+  	User user = User.findByEmail("bob@jones.com");
+  	Post post = user.posts.get(0);
+  	Comment comment1 = post.comments.get(0);
+  	Comment comment2 = post.comments.get(1);
+  	
+  	//Check post number, title and content
+  	assertEquals(1, user.posts.size());
+  	assertEquals("Post Title 3", user.posts.get(0).title);
+  	assertEquals("The content", user.posts.get(0).content);
+  	
+  	//Check comments number, sender and content
+  	assertEquals(2, post.comments.size());
+  	assertEquals(joe, post.comments.get(0).sender);
+  	assertEquals(lee, post.comments.get(1).sender);
+  	assertEquals("Comment from Joe", post.comments.get(0).content);
+  	assertEquals("Comment from Lee", post.comments.get(1).content);
+  	
+  	//Deleting comment&post from user and database
+  	post.comments.remove(comment1);
+  	post.comments.remove(comment2);
+  	
+    post.save();
+    
+    comment1.delete();
+    comment2.delete();
+  	
+  	user.posts.remove(post);
+  	
+  	user.save();
   	
   	post.delete();
   }
